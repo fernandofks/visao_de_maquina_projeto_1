3ebd7b8adcb9489325020b2ec156f250ca7d2dda
Giovanni <giovanniar@al.insper.edu.br>
2023-04-05 17:13:06 -0300

    Treshold

--- Tarefa4.py
+++ Tarefa4.py
@@ -3,15 +3,10 @@ import cv2
 
 import matplotlib.pyplot as plt
 from math import *
-from funcao_filtro import filtro
 
-flag0 = cv2.IMREAD_GRAYSCALE
-#Load an image in grayscale mode. Alternatively, we can pass integer value 0 for this flag
-img_gray = cv2.imread("img/Fig_OK_01.jpg", flag0) #or simply (path, 0)
-#Ler forma da figura (altura e largura)
-(h, w) = img_gray.shape
 
-cv2.imshow('graycsale image',img_gray)
+img_in = cv2.imread("visao_de_maquina_projeto_1\img\Fig_OK_01.jpg", cv2.IMREAD_COLOR)
+img_bin = np.where(img_in>80, 255, 0).astype("uint8")
+
+cv2.imshow('graycsale image',img_bin)
 cv2.waitKey(0)
-
-circulo=np.where(img_gray>150,255,0).astype(np.uint8)
