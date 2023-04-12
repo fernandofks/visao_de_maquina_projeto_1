235e5b60946e39a9c226dc5e8e1c1f3c46bc14e8
Giovanni <giovanniar@al.insper.edu.br>
2023-04-12 18:10:06 -0300

    Detect_Contornos_OK

--- Leitura_Pastas.py
+++ Leitura_Pastas.py
@@ -15,8 +15,8 @@ from filtros_extras import *
 
 #Parte 1: Importando e lendo todos os arquivos em uma pasta:
 
-#path = "OK"
-path = "NOK_borda"
+path = "OK"
+#path = "NOK_borda"
 
 dir_list = os.listdir(path)
 
@@ -30,6 +30,7 @@ largura_da_esteira = 65 # mm
 df = pd.DataFrame()
   
 Nome_arquivo     = []
+Convexidade_valor= [] 
 Teste_borda      = []
 Teste_superficie = []
 Diametro_Peca    = []
@@ -41,6 +42,7 @@ Status_Raio_AB   = []
 f = plt.figure(figsize=(10,5))
 for i in range(0,len(dir_list)):
     #encontrar uma maneira mais bonita de fazer a soma das strings abaixo
+    contours = []
     img_in = cv2.imread(path + "/" + str(dir_list[i]), cv2.IMREAD_COLOR)
     if img_in is None:
         print("File not found. Bye!")
@@ -48,7 +50,7 @@ for i in range(0,len(dir_list)):
     
     #Separando os canais de cor da imagem original
     [B,G,R] = cv2.split(img_in)
-    (height,width) = G.shape
+    (height,width) = B.shape
     
     #O filtro de grayscale especial está apresentando erros, provavelmente um overshoot do range (0,255), verificar se há astype("int32") 
     #img_in = grayscale_especial(img_in,const_red=0.00,const_green=1.00,const_blue=0.00)
@@ -57,11 +59,11 @@ for i in range(0,len(dir_list)):
     
     #plotar a imagem em um gráfico
     #ax = f.add_subplot (3,5,i+1)
-    #plt.imshow(G, cmap='gray')
+    #plt.imshow(B, cmap='gray')
     ##### Parte 1 resultados: Funcionando OK até aqui
     
     ##### Parte 2: Rodar um dos testes com a imagem (teste escolhido: Contorno externo)
-    returns,thresh=cv2.threshold(G,70,255,cv2.THRESH_BINARY)
+    returns,thresh=cv2.threshold(B,70,255,cv2.THRESH_BINARY_INV)
     thresh =  close (thresh)
     
     #usado para mostrar os contornos binarizados
@@ -71,35 +73,42 @@ for i in range(0,len(dir_list)):
     #convexity is calculated by the ratio of blob area/blob's convex hull area 
     # Get the convex hull of the contour
     contours,hierachy=cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    img1_text = cv2.cvtColor(B,cv2.COLOR_GRAY2RGB)
     
-    lista_perimetro =[]
+    #print("numero_contornos", len(contours))
     
-    img1_text = cv2.cvtColor(G,cv2.COLOR_GRAY2RGB)
+    perimetro_max = -1
+    contorno_out = contours[0]
     
     for j in range(len(contours)):
-        #achar o perimetro maximo para gerar esse contorno
-        Perimetro = cv2.arcLength(contours[j],True)
+        #achar o perimetro perimetro_maximo para gerar esse contorno
+        perimetro = cv2.arcLength(contours[j],True)
+        if perimetro > perimetro_max:
+            perimetro_max = perimetro
+            contorno_out = contours[j]
+    
         #lista_perimetro.append(Perimetro)
         #Substituir por perimetro de contorno ao invés de comprimento da lista de contornos
-        if Perimetro >= 1100:  
-            cv2.drawContours(img1_text,[contours[j]],-1,(0,0,255),4)
-            hull = cv2.convexHull(contours[j])
-            cv2.drawContours(img1_text,hull,-1,(0,255,0),8)
+    
+    #if perimetro >= perimetro_max:  
+    cv2.drawContours(img1_text,contorno_out,-1,(0,0,255),4)
+    hull = cv2.convexHull(contorno_out)
+    cv2.drawContours(img1_text,hull,-1,(0,255,0),8)
+    
+    area_contorno = cv2.contourArea(contorno_out)
+    area_hull = cv2.contourArea(hull)
+    convexidade = area_contorno/area_hull
+    
+    if convexidade < 0.9812:
+        Teste_borda.append("Reprovado")
+    else:
+        Teste_borda.append("Aprovado")
             
-            area_contorno = cv2.contourArea(contours[j])
-            area_hull = cv2.contourArea(hull)
-            convexidade = area_contorno/area_hull
-            
-            if convexidade < 0.95:
-                Teste_borda.append("Peça ruim")
-            else:
-                Teste_borda.append("Peça boa")
-                
-            print ("Perimetro",[i], Perimetro)
-            print ("Area_Contorno", [i], area_contorno)
-            print ("Area_Hull", [i], area_hull)
-            print ("Convexidade",[i], convexidade ,"\n")
-            
+    print ("Perimetro",[i], perimetro_max)
+    print ("Area_Contorno", [i], area_contorno)
+    print ("Area_Hull", [i], area_hull)
+    print ("Convexidade",[i], convexidade ,"\n")
+    Convexidade_valor.append(convexidade)
             
     #aro_exterior = filtro_blobs(G,True,True,False,True,False,Color = 0,Area_min=10000,Area_max=200000,Convexity_min = 0.95,Convexity_max=1)
     #img_text = img_in
@@ -113,7 +122,8 @@ for i in range(0,len(dir_list)):
     #img1_with_KPs = cv2.drawKeypoints(img_text, aro_exterior, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
     ax = f.add_subplot (3,5,i+1)
-    plt.imshow(img1_text, cmap='gray')
+    plt.imshow(thresh, cmap='gray')
+    #plt.imshow(img1_text, cmap='gray')
                 
     #cv2.imshow('Convexity Detection', img1_text)
     #cv2.waitKey(0)
@@ -122,7 +132,8 @@ for i in range(0,len(dir_list)):
 plt.show()
 
 df["Nome do Arquivo"] = Nome_arquivo
-df["Tem convexidade"] = Teste_borda
+df["Convexidade medida"] = Convexidade_valor
+df["Teste de borda"] = Teste_borda
 print(df)
 
 #Parte 1(Funcionando Ok)
