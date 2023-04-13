b2798e354228735f9ca0c377b48fd0047863b368
Giovanni <giovanniar@al.insper.edu.br>
2023-04-13 16:53:24 -0300

    Conta_todas_as_imagens no video

--- import_video.py
+++ import_video.py
@@ -7,12 +7,15 @@ import matplotlib.pyplot as plt
 import pandas as pd # para desenhar/plotar as tabelas informando os status das peças em cada teste
 
 from filtros_extras import *
+from fillHoles import fillHoles
+
 
 # Load the video file
 cap = cv2.VideoCapture('Video1_Vedacao.mp4')
-count =  0
-#cap.set(cv2.CAP_PROP_POS_MSEC,180000)
+first_appearance = True
+cont_pecas = 0 
 
+f = plt.figure(figsize=(20,10))
 # Loop through the frames in the video
 while cap.isOpened():
     # Read the current frame
@@ -25,33 +28,53 @@ while cap.isOpened():
         
         # binarizando a imagem
         returns,thresh=cv2.threshold(B,70,255,cv2.THRESH_BINARY_INV)
-        thresh = close(thresh)
+        #thresh = close(thresh)
+        thresh = fillHoles(thresh)
         
         contours,hierachy=cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     
         img1_text = cv2.cvtColor(B,cv2.COLOR_GRAY2RGB)
         
         if len(contours) != 0:
-            Peca_no_frame = True
             perimetro_max = -1
-            contorno_out = contours[0]
-            for j in range(len(contours)):
-                #achar o perimetro perimetro_maximo para gerar esse contorno
-                perimetro = cv2.arcLength(contours[j],True)
-                if perimetro > perimetro_max:
-                    perimetro_max = perimetro
-                    contorno_out = contours[j]
+            contorno_out=max(contours, key=len)
+            if len(contorno_out)>=5: #necessita de no mínimo 5 para fazer fit ellipse
+                ellipse = cv2.fitEllipse(contorno_out)
+                (x, y), (MA, ma),angle =  ellipse
+
+                area_contorno = cv2.contourArea(contorno_out)
+                #print (area_contorno)
+                    
+                if y > 200 and y< 300 and first_appearance and area_contorno>30000:
+                    cont_pecas += 1
+                    print ([cont_pecas], y)
+                        
+                    first_appearance = False
+                    ax = f.add_subplot (4,6,cont_pecas)
+                        #plt.imshow(B, cmap='gray')
+                        #plt.imshow(thresh, cmap='gray')
+                    plt.imshow(img1_text, cmap='gray')
+                if area_contorno<30000 and y>400:
+                    first_appearance = True
+
+                     
+                #M = cv2.moments(contours[j])
+                #
+                #if M["m00"] >=1: #evitar que o video crashe por conta de divisão por 0
+                #    cX = (M["m10"] / M["m00"])
+                #    cY = (M["m01"] / M["m00"])
 
             cv2.drawContours(img1_text,contorno_out,-1,(0,0,255),4)
             hull = cv2.convexHull(contorno_out)
             cv2.drawContours(img1_text,hull,-1,(0,255,0),8)
+            #cv2.circle(img1_text, (cX, cY), 7, (255, 255, 255), -1)
     
             area_contorno = cv2.contourArea(contorno_out)
             area_hull = cv2.contourArea(hull)
             if area_hull >= 1: #evitar que o video crashe por conta de divisão por 0
                 convexidade = area_contorno/area_hull
         else:
-            Peca_no_frame = False
+            first_appearance = True
             
         # Display the resulting image
         cv2.imshow('Blob Detection', img1_text)
@@ -65,3 +88,5 @@ while cap.isOpened():
 # Release the video capture object and close all windows
 cap.release()
 cv2.destroyAllWindows()
+
+plt.show()
