753e6e3c99f450967c81bea5fdcdb4eef3d0de29
Giovanni <giovanniar@al.insper.edu.br>
2023-04-13 20:26:40 -0300

    Cortando_imagens_pro_algoritmo de machine learning

--- import_video.py
+++ import_video.py
@@ -8,13 +8,20 @@ import pandas as pd # para desenhar/plotar as tabelas informando os status das p
 
 from filtros_extras import *
 from fillHoles import fillHoles
+from random import randint # used to generate borders to off-limits cropped images
 
+from tensorflow import keras
 
 # Load the video file
 cap = cv2.VideoCapture('Video1_Vedacao.mp4')
-first_appearance = True
-cont_pecas = 0 
 
+#Flag that checks if the rubber in the image is new or not
+first_appearance = True
+
+cont_pecas = 0 
+conjunto_NOK_video=[]
+x_des,y_des = (int(508),int(486))
+ 
 f = plt.figure(figsize=(20,10))
 # Loop through the frames in the video
 while cap.isOpened():
@@ -34,7 +41,7 @@ while cap.isOpened():
         contours,hierachy=cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     
         img1_text = cv2.cvtColor(B,cv2.COLOR_GRAY2RGB)
-        
+        img1_text_R = R
         if len(contours) != 0:
             perimetro_max = -1
             contorno_out=max(contours, key=len)
@@ -43,17 +50,67 @@ while cap.isOpened():
                 (x, y), (MA, ma),angle =  ellipse
 
                 area_contorno = cv2.contourArea(contorno_out)
+                
+                
                 #print (area_contorno)
-                    
+                
+                #lowers the first_appearance flag    
                 if y > 200 and y< 300 and first_appearance and area_contorno>30000:
+                    
+                    #generating cropped images in order to make the model work
+                    #documentation used for this part: https://docs.opencv.org/3.4/dc/da3/tutorial_copyMakeBorder.html
+                    #documentation used for this part: https://stackoverflow.com/questions/55733086/opencv-how-to-overcrop-an-image
+                    src = img1_text
+                    borderType = cv2.BORDER_REPLICATE
+                    boarderSize = .1
+                    top = int(boarderSize * src.shape[0])  # shape[0] = rows
+                    bottom = top
+                    left = int(boarderSize * src.shape[1])  # shape[1] = cols
+                    right = left    
+                    value = [randint(0,255), randint(0,255), randint(0,255)]
+                    dst = cv2.copyMakeBorder(src, top, bottom, left, right, borderType, None, value)
+                    
+                    
+                    #actually cropping the image (before making border it actually worked)
+                    #cv2.rectangle(src, (int(x - x_des/2), int(y - y_des/2)), (int(x + x_des/2),int(y + y_des/2)), (255, 0, 0), 3)
+                    #cropped_image = src[int(x - x_des/2):int(x + x_des/2), int(y - y_des/2):int(y + y_des/2)]
+                    
+                    #actually actually cropping the image
+                    #dst = cv2.cvtColor(dst,cv2.COLOR_BGR2GRAY)
+                    border_height = dst.shape[0]
+                    border_width = dst.shape[1]
+                    x_offset = -70
+                    y_offset = -70
+                    x_min_border = int((border_width - x_des)/2) + x_offset
+                    y_min_border = int((border_height - y_des)/2) + y_offset
+                    x_max_border = int((border_width + x_des)/2) + x_offset
+                    y_max_border = int((border_height  + y_des)/2) + y_offset
+                    
+                    #cv2.rectangle(dst, x_min_border, y_min_border, x_max_border, y_max_border, (255, 0, 0), 3)
+                    cv2.rectangle(dst, (int((border_width - x_des)/2), int((border_height - y_des)/2)+20), (int((border_width + x_des)/2),int((border_height + y_des)/2)+20), (255, 0, 0), 3)
+                    
+                    #o comando de crop image funciona na base de (y,x) e não (x,y) e se passaram 30 min até eu descobrir isso
+                    #documentação dessa merda: https://stackoverflow.com/questions/15589517/how-to-crop-an-image-in-opencv-using-python
+                    cropped_image = src[y_min_border: y_max_border, x_min_border: x_max_border]
+                    cv2.imshow("cropped", cropped_image)
+                    # Display cropped image
+                    #cv2.imshow("cropped", cropped_image)
+                    
                     cont_pecas += 1
                     print ([cont_pecas], y)
                         
                     first_appearance = False
+                    
                     ax = f.add_subplot (4,6,cont_pecas)
-                        #plt.imshow(B, cmap='gray')
-                        #plt.imshow(thresh, cmap='gray')
-                    plt.imshow(img1_text, cmap='gray')
+                    #plt.imshow(B, cmap='gray')
+                    #plt.imshow(thresh, cmap='gray')
+                    #plt.imshow(img1_text, cmap='gray')
+                    plt.imshow(dst, cmap='gray')
+                    conjunto_NOK_video.append(img1_text_R)
+                    
+                    
+                
+                #raises the first_appearance flag
                 if area_contorno<30000 and y>400:
                     first_appearance = True
 
@@ -68,7 +125,8 @@ while cap.isOpened():
             hull = cv2.convexHull(contorno_out)
             cv2.drawContours(img1_text,hull,-1,(0,255,0),8)
             #cv2.circle(img1_text, (cX, cY), 7, (255, 255, 255), -1)
-    
+            
+            
             area_contorno = cv2.contourArea(contorno_out)
             area_hull = cv2.contourArea(hull)
             if area_hull >= 1: #evitar que o video crashe por conta de divisão por 0
@@ -84,9 +142,12 @@ while cap.isOpened():
 
     else:
         break
-
+conjunto_NOK_video=np.stack(conjunto_NOK_video, axis=0)
 # Release the video capture object and close all windows
+#model=keras.models.load_model('./superficie.h5')
+#print(model.predict(conjunto_NOK_video))
 cap.release()
 cv2.destroyAllWindows()
 
 plt.show()
+
